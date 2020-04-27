04a283d3da261dbd48fab76b2c6cb7268a2ddd6f
mahesapama03adhiswara <mahesapamaadhiswara@gmail.com>
2020-04-27 12:51:41 +0800

    membuat array dengan pengurutan variable angka dengan pemilihan nilai urut array

---
+++ src/array_/array_main.java
@@ -0,0 +1,11 @@
+package array_;
+
+public class array_main {
+    public static void main(String[] args) {
+        int nilaiArr[] = {20,25,30,35,40,45,50,55,60,65,70};
+        System.out.println(nilaiArr.length);
+        System.out.println(nilaiArr[0]);
+        System.out.println(nilaiArr[8]);
+
+    }
+}
