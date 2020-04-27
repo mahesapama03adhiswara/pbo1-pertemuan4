cdf5fff27d24f9589b0c80f4a28eeb288fcfe6c8
mahesapama03adhiswara <mahesapamaadhiswara@gmail.com>
2020-04-27 12:54:46 +0800

    membuat array dengan pengurutan variable angka dengan pemilihan variable

---
+++ src/array_/array2.java
@@ -0,0 +1,15 @@
+package array_;
+
+public class array2 {
+    public static void main(String[] args) {
+        String mataKuliah[];
+        mataKuliah = new String[10];
+
+        mataKuliah[0]= "pemograman Berbasis Objek 1";
+        mataKuliah[1]= "Visual 2";
+        mataKuliah[2]= "Rekayasa Perangkat Lunak";
+        mataKuliah[3]= "Organisasi Komputer";
+        mataKuliah[4]= "Pemograman WEB 2";
+
+    }
+}
