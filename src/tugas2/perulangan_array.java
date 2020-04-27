5a44a91cb7f2dc38b647921512ddd8242ecbb0ca
mahesapama03adhiswara <mahesapamaadhiswara@gmail.com>
2020-04-27 14:50:18 +0800

    perulaangan array dengan bilanan kuadrat

---
+++ src/tugas2/perulangan_array.java
@@ -0,0 +1,17 @@
+package tugas2;
+
+public class perulangan_array {
+    public static void main(String[] args) {
+        int[] bilangan = new int[15];
+        int kuadrat;
+        for (int i = 0; i < bilangan.length; i++) {
+            bilangan[i] = i + 1;
+        }
+        for (int i = 0; i < bilangan.length; i++) {
+            if (bilangan[i] % 2 != 0) {
+                kuadrat = bilangan[i] * bilangan[i];
+                System.out.println(bilangan[i] + " kuadrat = " + kuadrat);
+            }
+        }
+    }
+}
